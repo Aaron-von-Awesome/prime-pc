0e4e3fd1238b4a06ca59d298959a4d7d6a6247b1
Aaron von Awesome <codeberg@vonawesome.email>
2025-03-25 23:06:58 -0400

    Update Roles and Collections

--- collections/ansible_collections/community/general/plugins/module_utils/redfish_utils.py
+++ collections/ansible_collections/community/general/plugins/module_utils/redfish_utils.py
@@ -3970,3 +3970,20 @@ class RedfishUtils(object):
             'ret': True,
             'entries': response['data']
         }
+
+    def get_power_restore_policy(self, systems_uri):
+        # Retrieve System resource
+        response = self.get_request(self.root_uri + systems_uri)
+        if response['ret'] is False:
+            return response
+        return {
+            'ret': True,
+            'entries': response['data']['PowerRestorePolicy']
+        }
+
+    def get_multi_power_restore_policy(self):
+        return self.aggregate_systems(self.get_power_restore_policy)
+
+    def set_power_restore_policy(self, policy):
+        body = {'PowerRestorePolicy': policy}
+        return self.patch_request(self.root_uri + self.systems_uri, body, check_pyld=True)
